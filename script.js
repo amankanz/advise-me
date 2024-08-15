2ecd3ffb53b403faf7935d67845d6e4cc5b0d9b0
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-15 15:49:11 +0300

    complete: fetching feature completed

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+document.getElementById("loader").hidden = true;
+
+function loading() {
+  document.getElementById("loadingAdvice").hidden = true;
+  document.getElementById("loader").hidden = false;
+}
+
+function completeLoading() {
+  document.getElementById("loadingAdvice").hidden = false;
+  document.getElementById("loader").hidden = true;
+}
+
 async function getAdvice() {
+  loading();
   try {
     const response = await fetch("https://api.adviceslip.com/advice", {
       header: {
@@ -7,8 +20,11 @@ async function getAdvice() {
     });
 
     const adviceData = await response.json();
+
     document.getElementById("loadingAdvice").textContent =
       adviceData.slip.advice;
+
+    completeLoading();
     console.log(adviceData);
   } catch (error) {
     console.log(error);
